dbd78fb181aa1ba89bcc42d001c72c7fd7bcdfca
Samuel Kubai <kamausamuel11@gmail.com>
2015-09-08 09:59:10 +0300

    Set up default blade template

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+get('/', function () {
+    return view('ba.home.index');
 });
---
+++ resources/views/ba/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <h1> Welcome to Bongo Afrika </h1>
+
+    <p> <b> (Default blade page) </b></p>
+@stop
