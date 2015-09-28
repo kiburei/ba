0cba5b42cff7782ae1d01748a11622d6c19bdbb3
iroshbrian <irungu@wearearchangel.com>
2015-09-28 13:49:29 +0300

    Initialize repo with design files

--- resources/views/dashboards/innovator.blade.php
+++ resources/views/dashboards/innovator.blade.php
@@ -1,3 +1,157 @@
-I am an innovator</br>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Homepage-Innovator</title>
 
-<a href="/logout">Logout</a>
+
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Bitter:700,400,400italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap-theme.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/homepage-innovator.css') }}">
+
+</head>
+
+<body>
+	<div class="row">
+	        <div class="col-lg-9">
+	            <div class="row">
+	                <div  class="title-pane">
+	                    <div class="pull-left">
+	                        <h1>Innovation title</h1>
+	                    </div>
+	                    <div class="pull-right">
+	                        <a href="post-innovation.html" class="anchor"><div class="btn btn-success">
+	                           <h3>Start</h3>
+	                        </div></a> 
+	                    </div>
+	                </div> <!-- end title-pane -->
+	            </div>          
+	        
+	            <div class="inovations-pane">
+	                <div class="row row-Innovation">
+	                    <div class="col-lg-6 six">
+	                        <h4 class="innovation-title">
+	                            <a href="">Creative name for the Innovation</a>
+	                        </h4>
+	                        <h5>
+	                            <a href="">Category</a>
+	                        </h5>
+	                        <p class="innovation-p">
+	                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	                            consequat.
+	                        </p>
+	                    </div> <!-- end col-lg-6 -->
+
+	                    <div class="col-lg-6 six">
+	                        <h4>
+	                            <a href="" class="innovation-title"> Creative name for the Innovation</a>
+	                        </h4>
+	                        <h5>
+	                            <a href="">Category</a>
+	                        </h5>
+	                        <p class="innovation-p">
+	                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	                            consequat.
+	                        </p>
+	                    </div> <!-- end col-lg-6 --> 
+	                </div> <!-- end row-innovation -->
+	                
+	                <div class="row row-Innovation">
+	                    <div class="col-lg-6 six">
+	                        <h4> 
+	                            <a href="" class="innovation-title">Creative name for the Innovation
+	                            </a>
+	                        </h4>
+	                        <h5><a href="">Category</a></h5>
+	                        <p class="innovation-p">
+	                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	                            consequat.
+	                        </p>
+	                    </div> <!-- end col-lg-6 -->
+
+	                    <div class="col-lg-6 six">
+	                       <h4> <a href="" class="innovation-title">Creative name for the Innovation</a></h4>
+	                        <h5><a href="">Category</a></h5>
+	                        <p class="innovation-p">
+	                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	                            consequat.
+	                        </p>
+	                    </div> <!-- end col-lg-6 --> 
+	                </div> <!-- end row-innovation -->
+	                
+	                <div class="row row-Innovation">
+	                    <div class="col-lg-6 six">
+	                        <h4> <a href=""  class="innovation-title">Creative name for the Innovation</a></h4>
+	                        <h5><a href="">Category</a></h5>
+	                        <p class="innovation-p">
+	                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	                            consequat.
+	                        </p>
+	                    </div> <!-- end col-lg-6 -->
+
+	                    <div class="col-lg-6 six">
+	                        <h4> <a href=""  class="innovation-title">Creative name for the Innovation</a></h4>
+	                        <h5><a href="">Category</a></h5>
+	                        <p class="innovation-p">
+	                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	                            consequat.
+	                        </p>
+	                    </div> <!-- end col-lg-6 --> 
+	                </div> <!-- end row-innovation -->
+	                
+	            </div> <!-- end innovations-pane -->
+	        
+
+	</div> <!-- end col-lg-9 -->
+
+	    <div class="col-lg-3">
+	    	
+	            <div class="advert well"></div>
+	            <hr />
+	            <div class="funded-projects">
+	                <h2>Funded Projects</h2>
+	                
+	                    <a href="">Lorem ipsum dolor sit amet,consectetur adipiscing elit.</a>
+	                    <h5>Category</h5> 
+	                    
+	                    <hr />
+	                    <a href="">Lorem ipsum dolor sit amet,consectetur adipiscing elit.</a>
+	                    <h5>Category</h5>
+	                    
+	                    <hr />
+	                    <a href="">Lorem ipsum dolor sit amet,consectetur adipiscing elit.</a>
+	                    <h5>Category</h5>
+	                    
+	                    <hr />
+	                    <a href="">Lorem ipsum dolor sit amet,consectetur adipiscing elit.</a>
+	                    <h5>Category</h5>
+	                    
+	                
+	            </div>
+	    	
+	    </div> <!-- end col-lg-3 -->
+	    
+	</div>
+
+<!-- compiled and minified javascript -->
+<script src="{{ asset('/js/jquery.min.js') }}"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+
+</body>
+</html>
+
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,66 +4,63 @@
     <title>Bongo Afrika</title>
 
     <!-- Latest compiled and minified CSS -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Bitter:700,400,400italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap-theme.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/homepage-innovator.css') }}">
 
 
 </head>
 <body>
 
 
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Bongo Afrika</a>
+    <div class="container" id="main">
+
+              <div class="navbar navbar-default barnav">
+                
+                <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-main" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Bongo afrika</a>
+                    </div>
+
+                    <div class="navbar-collapse collapse navbar-responsive-collapse" id="navbar-main">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="">Home</a></li>
+                            <li><a href="">About</a></li>
+                            <li><a href="">Team</a></li>
+                            <li class=""><a href="">Contact Us</a></li>
+                        </ul>
+                        <form class="navbar-form pull-left">
+                            <input type="text" class="form-control" placeholder="Search this site..." id="search-input">
+                            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+                        </form>
+                        
+                        
+                        <ul class="nav navbar-nav navbar-right">
+                        @if(\Auth::user())
+                        <li><a href="{{ url('/logout') }}">Logout</a></li>
+                        <li><a><b>Signed in as {{ \Auth::user()->name }}</b></a></li>
+                        @else
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        @endif
+                        </ul>
+                    </div> <!-- end nav-collapse -->
+              </div> <!-- end navbar -->
+
+
+    <div>
+        @yield('content')
     </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-      @if(\Auth::user())
-        <li><a href="{{ url('/logout') }}">Logout</a></li>
-        <li><a><b>Signed in as {{ \Auth::user()->name }}</b></a></li>
-      @else
-        <li><a href="{{ url('/login') }}">Login</a></li>
-        <li><a href="{{ url('/register') }}">Register</a></li>
-      @endif
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
-<div class="container">
-    @yield('content')
-</div>
+  </div> <!-- end container main-->
 
 
 <!-- compiled and minified javascript -->
