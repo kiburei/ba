4a6d5f4b146fa9e95c1e3c791719a123736f9e36
michaelwmwaura <mail@michaelmwaura.co.ke>
2015-10-08 23:55:01 +0300

    Fix php version

--- deploy.sh
+++ deploy.sh
@@ -70,7 +70,6 @@ fi
 # ----------
 echo Downloading Composer
 curl -sS https://getcomposer.org/installer | php
-Add the below after the Deployment section
 
 
 ##################################################################################################################################
